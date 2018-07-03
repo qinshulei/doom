34a4416f82b1ff79c60c180cbd2ad6180af21577
Ryan Phillips <rphillips@redhat.com>
2018-07-02 19:40:45 -0500

    use iosevka term

--- +ui.el
+++ +ui.el
@@ -5,8 +5,8 @@
 
 (setq ns-use-proxy-icon nil)
 
-(setq doom-font (font-spec :family "Iosevka" :size 13))
-(setq doom-big-font (font-spec :family "Iosevka" :size 26))
+(setq doom-font (font-spec :family "Iosevka Term" :size 13))
+(setq doom-big-font (font-spec :family "Iosevka Term" :size 26))
 (setq doom-molokai-brighter-comments t)
 (setq doom-theme 'doom-one-light)
 
